7c6a91472b34d39ab2fc2e384c8bc60c777f7a0a
williamvianna <viannawp@gmail.com>
2023-08-31 20:29:24 -0300

    Criado js cartaoProduto, menuCarrinho e utilidades

--- magazine-hashtag/src/menuCarrinho.js
+++ magazine-hashtag/src/menuCarrinho.js
@@ -1,3 +1,5 @@
+import { catalogo } from "./utilidades";
+
 function abrirCarrinho() {
     document.getElementById("carrinho").classList.add("right-[0px]");
     document.getElementById("carrinho").classList.remove("right-[-360px]");
@@ -14,4 +16,25 @@ export function inicializarCarrinho() {
 
     botaoFecharCarrinho.addEventListener("click", fecharCarrinho);
     botaoAbrirCarrinho.addEventListener("click", abrirCarrinho);
+}
+
+export function adcionarAoCarrinho(idProduto) {
+    const produto = catalogo.find((p) => p.id === idProduto);
+    const containerProdutosCarrinho = document.getElementById("produtos-carrinho");
+    const cartaoProdutoCarrinho = `<article class="flex bg-slate-100 rounded-lg p-1 relative">
+    <button id="fechar-carrinho" class="absolute top-0 right-2"><i class="fa-solid fa-circle-xmark text-slate-500 hover:text-slate-800"></i></button>
+    <img src="./img/${produto.imagem}" alt="Carrinho: ${produto.nome}" class="h-24 rounded-lg">
+    <div class="p-2 flex-col justify-between">
+      <p class="text-slate-900 text-sm">${produto.nome}</p>
+      <p class="text-slate-400 text-xs">Tamanho: M</p>
+      <p class="text-green-700 text-lg">$${produto.preco}</p>
+    </div>
+
+    <div>
+    <button></button>
+    <p>2</p>
+    <button></button>
+    </div>
+  </article>`;
+  containerProdutosCarrinho.innerHTML += cartaoProdutoCarrinho;
 }
