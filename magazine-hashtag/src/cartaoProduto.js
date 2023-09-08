1dbf18c5da50c0f503c104d4785943f1d6ebdb33
williamvianna <viannawp@gmail.com>
2023-09-08 20:55:12 -0300

    Filtos

--- magazine-hashtag/src/cartaoProduto.js
+++ magazine-hashtag/src/cartaoProduto.js
@@ -3,7 +3,7 @@ import { catalogo } from "./utilidades";
 
 export function renderizarCatalogo() {
     for (const produtoCatalogo of catalogo) {
-        const cartaoProduto = `<div class="border-solid w-48 m-2 flex flex-col p-2 justify-between shadow-xl shadow-slate-400 rounded-lg group" id="card-produto-${produtoCatalogo.id}">
+        const cartaoProduto = `<div class="border-solid w-48 m-2 flex flex-col p-2 justify-between shadow-xl shadow-slate-400 rounded-lg group ${produtoCatalogo.feminino ? "feminino" : "masculino"}" id="card-produto-${produtoCatalogo.id}">
         <img src="./img/${produtoCatalogo.imagem}" alt="Produto 1 do Magazine Hashtag." class="group-hover:scale-110 duration-300 my-3 rounded-lg">
         <p class="text-sm">${produtoCatalogo.marca}</p>
         <p class="text-sm">${produtoCatalogo.nome}</p>
