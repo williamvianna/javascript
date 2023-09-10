b1e5ac2e219b99f4383f19d806f337f90e9b22b3
williamvianna <viannawp@gmail.com>
2023-09-10 18:58:01 -0300

    Atualização

--- Magazine WPV/vite.config.js
+++ Magazine WPV/vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/magazineIntensivao/",
+  base: "/MagazineWPV/",
   plugins: [],
   resolve: {
     /*something*/
